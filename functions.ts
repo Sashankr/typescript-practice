39f1eafe3760cb2d045b144fd56fc9ab7b12896d
Sashankr <sashankr97@gmail.com>
2026-01-01 23:45:10 +0530

    inferred return type

--- functions.ts
+++ functions.ts
@@ -5,3 +5,11 @@ function calculateTotal(
 ) : number {
   return price * quantity * (1 - discount)
 }
+
+// Inferred return type, adding just the types for parameters in functions is enough, typescript can infer what the returned type can be based on this
+
+function prompt(systemPrompt:string,userPrompt:string){
+  return `${systemPrompt}\n${userPrompt}`
+}
+
+console.log(prompt('You are a helpful assistant','how to reverse a string?'));
