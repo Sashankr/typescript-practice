305c2e8a3cfad62334e79a4f52c59c2ab7ce1d99
Sashankr <sashankr97@gmail.com>
2026-01-04 22:16:41 +0530

    union type

--- functions.ts
+++ functions.ts
@@ -33,3 +33,17 @@ function setLoggerTimeout(loggerCallback:LoggerCallback,delay:number){
 // We can import types from external files with the help of type keyword, what this does is, it reduces bundle size when generating production build
 // This is because types are not required in production, these are only necessary to debug while writing our code.
 // ex : import type { User, Role } from './models' 
+//
+
+function processTicketId(ticketId: string | number){
+  if(typeof ticketId === 'string'){
+    ticketId = ticketId.split('-')?.[1];
+    ticketId = parseInt(ticketId,10);
+  }
+  return `Ticket ID ${ticketId} is being processed`;
+}
+
+const ticketResponse = processTicketId('Support-124');
+console.log(ticketResponse);
+
+
