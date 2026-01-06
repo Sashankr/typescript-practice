a038b4423e71c364476fe12288fff658e5f37113
Sashankr <sashankr97@gmail.com>
2026-01-06 12:04:43 +0530

    array type syntax

--- arrays.ts
+++ arrays.ts
@@ -1,2 +1,9 @@
 const userList : number[] = [15,12,155,22]; // number array;
 const usernames : string[] = ['sashank','joel']; // string array
+
+
+// Array type syntax
+type listOfUsers = string[];
+type listOfActiveUsers = Array<string>;
+
+
