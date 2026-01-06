b74231048b5405039d6dbfcc2dbca5fc3930dee5
Sashankr <sashankr97@gmail.com>
2026-01-06 12:55:41 +0530

    evolving any type

--- arrays.ts
+++ arrays.ts
@@ -1,6 +1,5 @@
-const userList : number[] = [15,12,155,22]; // number array;
-const usernames : string[] = ['sashank','joel']; // string array
-
+const userList: number[] = [15, 12, 155, 22]; // number array;
+const usernames: string[] = ["sashank", "joel"]; // string array
 
 // Array type syntax
 type listOfUsers = string[];
@@ -8,12 +7,18 @@ type listOfActiveUsers = Array<string>;
 
 // Heterogenous Arrays, combination of 2 different types of values
 
-type comments = (string | number) [];
+type comments = (string | number)[];
 
-function userInfo(admin,...remaining){
+function userInfo(admin: string, ...remaining: string[]) {
   console.log(admin);
   console.log(remaining);
 }
 
-userInfo('sashank','joel','henry');
+userInfo("sashank", "joel", "henry");
 
+const list = [];
+list.push(10);
+list.push("hello");
+
+const copy = list;
+copy.push(true);
