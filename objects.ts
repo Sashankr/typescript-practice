9fde3640473b0cdf5a0aa181e623e35acb916934
Sashankr <sashankr97@gmail.com>
2026-02-01 21:44:06 +0530

    object literal type

--- objects.ts
+++ objects.ts
@@ -2,3 +2,9 @@ type Car = {
   model : string,
   topSpeed : number
 }
+
+function carInfo(details:Car){
+  console.log(`Model : ${details.model}, Top Speed : ${details.topSpeed}`);
+}
+
+carInfo({model : '911',topSpeed:250});
