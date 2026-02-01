7f3a55129415e9d78ec56352634db2e06577a09c
Sashankr <sashankr97@gmail.com>
2026-02-01 21:52:40 +0530

    issue with passing direct object literal with extra properties

--- objects.ts
+++ objects.ts
@@ -1,10 +1,21 @@
 type Car = {
   model : string,
-  topSpeed : number
+  topSpeed : number,
 }
 
 function carInfo(details:Car){
   console.log(`Model : ${details.model}, Top Speed : ${details.topSpeed}`);
 }
 
-carInfo({model : '911',topSpeed:250});
+const car = {
+  name : 'Porsche',
+  model : '911',
+  topSpeed : 250,
+}
+
+carInfo(car);
+
+// When we try to pass an object with extra property, its not allowed,but if passed as variable its allowed.
+// Object literal may only specify known properties, and 'name' does not exist in type 'Car'.
+
+carInfo({name : 'Porsche',model:'911',topSpeed:250}); 
